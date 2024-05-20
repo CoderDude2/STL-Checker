7a07e6d075fd5b14b6fc7a1857927b63988e5393
Isaac Boots <isaacboots1@gmail.com>
2024-05-19 23:16:16 -0700

    Figured out how to calculate t.

--- case.py
+++ case.py
@@ -21,30 +21,31 @@ class Case:
 
 def get_cases(folder_path:str) -> list[Case]:
     cases:list[Case] = []
-    for file in os.listdir(folder_path):
-        file_name = case_regex.match(file)
-        if(file_name):
-            stl_file:stl.STLObject = stl.open_stl_file(os.path.join(folder_path, file_name.group(0)))
-            circle:str = ""
-            case_type:str = ""
-            max_length:float = 14.2 if any([i in file_name.group("connection_type") for i in fourteen_millimeter]) else 17.2
+    if(os.path.exists(folder_path)):
+        for file in os.listdir(folder_path):
+            file_name = case_regex.match(file)
+            if(file_name):
+                stl_file:stl.STLObject = stl.open_stl_file(os.path.join(folder_path, file_name.group(0)))
+                circle:str = ""
+                case_type:str = ""
+                max_length:float = 14.2 if any([i in file_name.group("connection_type") for i in fourteen_millimeter]) else 17.2
 
-            if("TA14" in file_name.group("connection_type")):
-                circle = "14pi"
-            elif("TA10" in file_name.group("connection_type")):
-                circle = "10pi"
-            else:
-                circle = "14pi"
+                if("TA14" in file_name.group("connection_type")):
+                    circle = "14pi"
+                elif("TA10" in file_name.group("connection_type")):
+                    circle = "10pi"
+                else:
+                    circle = "14pi"
 
-            if("T-L" in file_name.group("connection_type")):
-                case_type = "TLOC"
-            elif("AOT" in file_name.group("connection_type")):
-                case_type = "AOT"
-            elif("ASC" in file_name.group("connection_type")):
-                case_type = "ASC"
-            else:
-                case_type = "DS"
+                if("T-L" in file_name.group("connection_type")):
+                    case_type = "TLOC"
+                elif("AOT" in file_name.group("connection_type")):
+                    case_type = "AOT"
+                elif("ASC" in file_name.group("connection_type")):
+                    case_type = "ASC"
+                else:
+                    case_type = "DS"
 
-            c:Case = Case(stl_file, file_name.group("PDO"), file_name.group("connection_type"), circle, case_type, max_length)
-            cases.append(c)
+                c:Case = Case(stl_file, file_name.group("PDO"), file_name.group("connection_type"), circle, case_type, max_length)
+                cases.append(c)
     return cases
--- checks.py
+++ checks.py
@@ -1,10 +1,22 @@
 import numpy as np
 import numpy.typing as npt
 
-def intersect_plane(plane_normal:npt.ArrayLike, plane_origin:npt.ArrayLike, ray_origin:npt.ArrayLike, ray_direction:npt.ArrayLike, t:float) -> bool:
-    denominator:float = np.dot(plane_normal, ray_direction)
-    if(denominator > 1e-6):
-        p0l0:npt.ArrayLike = np.subtract(plane_origin, ray_origin)
-        t = np.dot(p0l0, plane_normal) / denominator
-        return (t >= 0)
-    return False
+def normalize(vector:npt.ArrayLike) -> npt.ArrayLike:
+    return vector / np.sqrt(np.dot(vector, vector))
+
+
+def intersect_plane(plane_point:npt.ArrayLike, plane_normal:npt.ArrayLike, ray_origin:npt.ArrayLike, ray_direction:npt.ArrayLike):
+    ray_direction = np.round(normalize(ray_direction - ray_origin), 4)
+    print(ray_direction)
+    if(ray_direction.dot(plane_normal) > 1e-6):
+        t = np.round(((plane_point - ray_origin).dot(plane_normal)) / (ray_direction.dot(plane_normal)), 4)
+        print(np.round(ray_origin + (ray_direction * t),4))
+        print(t)
+
+
+print(intersect_plane(
+    plane_normal=np.array([0,0,1]),
+    plane_point=np.array([0,0,0]),
+    ray_origin=np.array([1,1,-2]),
+    ray_direction=np.array([0,0,1]),
+))
