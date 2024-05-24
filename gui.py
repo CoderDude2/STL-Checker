6e14a213a0218b335ee7eee58b1870ce8d2e7563
CoderDude2 <isaacboots1@gmail.com>
2024-05-23 17:40:58 -0700

    Added labels and a button to gui.py

--- gui.py
+++ gui.py
@@ -10,6 +10,19 @@ class App(tk.Tk):
 
         self.process_files_btn = tk.Button(text="Process Files")
 
+        self.uncentered_lbl = tk.Label(text="Uncentered: 0")
+        self.exceeds_10_pi_lbl = tk.Label(text="Exceeds 10pi: 0")
+        self.exceeds_14_pi_lbl = tk.Label(text="Exceeds 14pi: 0")
+        self.exceeds_max_length_lbl = tk.Label(text="Exceeds Max Length: 0")
+        self.passed_lbl = tk.Label(text="Passed: 0")
+
+        self.process_files_btn.pack(fill="x")
+
+        self.uncentered_lbl.pack(anchor="w")
+        self.exceeds_10_pi_lbl.pack(anchor="w")
+        self.exceeds_14_pi_lbl.pack(anchor="w")
+        self.exceeds_max_length_lbl.pack(anchor="w")
+        self.passed_lbl.pack(anchor="w")
 
 app = App()
 app.mainloop()
