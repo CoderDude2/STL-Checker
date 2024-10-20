5e22173bbcd2a90ce0a020da18cc7c7358f4f5e6
Isaac Boots <isaacboots1@gmail.com>
2024-10-20 16:47:48 -0700

    Added a basic thread to update counters with number of .stls in the respective folders

--- gui.py
+++ gui.py
@@ -44,15 +44,8 @@ if(not os.path.exists(PASSED_PATH)):
 file_processing_lock:threading.Lock = threading.Lock()
 
 class Checker(threading.Thread):
-    def __init__(self, counter_callback:callable=None) -> None:
+    def __init__(self) -> None:
         threading.Thread.__init__(self, daemon=True)
-        self.counter_callback:callable = counter_callback
-        self.uncentered_count:int = 0
-        self.exceeds_10pi_count:int = 0    
-        self.exceeds_14pi_count:int = 0
-        self.exceeds_max_length_count:int = 0
-        self.passed_count:int = 0
-
         self.is_checking:bool = False
 
     def run(self) -> None:
@@ -60,13 +53,6 @@ class Checker(threading.Thread):
             self.process_files()
 
     def start_processing(self) -> None:
-        if(self.is_checking == False):
-            self.uncentered_count = 0
-            self.exceeds_10pi_count = 0
-            self.exceeds_14pi_count = 0
-            self.exceeds_max_length_count = 0
-            self.passed_count = 0
-            self.counter_callback()
         self.is_checking = True if self.is_checking == False else self.is_checking
         
     def process_files(self) -> None:
@@ -81,45 +67,41 @@ class Checker(threading.Thread):
                                     os.path.join(FILES_PATH, c.name),
                                     os.path.join(UNCENTERED_PATH, c.name)
                                     ) 
-                            self.uncentered_count += 1
                         elif(not c.stl.in_circle_10pi() and c.circle == "10pi"):
                             shutil.move(
                                 os.path.join(FILES_PATH, c.name),
                                 os.path.join(OVER_10_PI_PATH, c.name)
                                 )
-                            self.exceeds_10pi_count += 1
                         elif(not c.stl.in_circle_14pi() and c.circle == "14pi"):
                             shutil.move(
                                 os.path.join(FILES_PATH, c.name),
                                 os.path.join(OVER_14_PI_PATH, c.name)
                             )
-                            self.exceeds_14pi_count += 1
                         elif(c.stl.length() > c.max_length):
                             shutil.move(
                                 os.path.join(FILES_PATH, c.name),
                                 os.path.join(EXCEEDS_MAX_LENGTH_PATH, c.name)
                             )
-                            self.exceeds_max_length_count += 1
                         else:
                             shutil.move(
                                 os.path.join(FILES_PATH, c.name),
                                 os.path.join(PASSED_PATH, c.name)
                             )
-                            self.passed_count += 1
-                        self.counter_callback()
                     except FileNotFoundError:
-                        pass
+                        print("Could not find file", c.name)
                 self.is_checking = False
 
 class App:
     def __init__(self) -> None:
         self.master:tk.Tk = tk.Tk()
         self.master.title("STL Checker")
-        # self.master.geometry("250x200")
         self.master.protocol("WM_DELETE_WINDOW", self.on_close)
 
-        self.c:Checker = Checker(self.update_counter_request)
+        self.update_counters_thread = threading.Thread(target=self.update_counters, daemon=True)
+
+        self.c:Checker = Checker()
         self.c.start()
+        self.update_counters_thread.start()
 
         self.uncentered_text:tk.StringVar = tk.StringVar(value="Uncentered: 0")
         self.exceeds_10_pi_text:tk.StringVar = tk.StringVar(value="Exceeds 10pi: 0")
@@ -147,17 +129,22 @@ class App:
         self.add_files_btn.pack(fill="x")
         self.process_files_btn.pack(fill="x")
         self.label_frame.pack(fill="x", pady=20)
-        self.open_output_btn.pack(fill="x")
+        self.open_output_btn.pack(fill="x", side=tk.BOTTOM)
 
-    def update_counter_request(self) -> None:
-        self.update_counters(self.c.uncentered_count, self.c.exceeds_10pi_count, self.c.exceeds_14pi_count, self.c.exceeds_max_length_count, self.c.passed_count)
 
-    def update_counters(self, uncentered_count:int, exceeds_10pi_count:int, exceeds_14pi_count:int, exceeds_length_count:int, passed_count:int) -> None:
-        self.uncentered_text.set(f"Uncentered: {uncentered_count}")
-        self.exceeds_10_pi_text.set(f"Exceeds 10pi: {exceeds_10pi_count}")
-        self.exceeds_14_pi_text.set(f"Exceeds 14pi: {exceeds_14pi_count}")
-        self.exceeds_max_length_text.set(f"Exceeds Max Length: {exceeds_length_count}")
-        self.passed_text.set(f"Passed: {passed_count}")
+    def update_counters(self) -> None:
+        while True:
+            uncentered_count = len([file for file in os.listdir(UNCENTERED_PATH) if ".stl" in file.lower()])
+            exceeds_10pi_count = len([file for file in os.listdir(OVER_10_PI_PATH) if ".stl" in file.lower()])
+            exceeds_14pi_count = len([file for file in os.listdir(OVER_14_PI_PATH) if ".stl" in file.lower()])
+            exceeds_length_count = len([file for file in os.listdir(EXCEEDS_MAX_LENGTH_PATH) if ".stl" in file.lower()])
+            passed_count = len([file for file in os.listdir(PASSED_PATH) if ".stl" in file.lower()])
+            
+            self.uncentered_text.set(f"Uncentered: {uncentered_count}")
+            self.exceeds_10_pi_text.set(f"Exceeds 10pi: {exceeds_10pi_count}")
+            self.exceeds_14_pi_text.set(f"Exceeds 14pi: {exceeds_14pi_count}")
+            self.exceeds_max_length_text.set(f"Exceeds Max Length: {exceeds_length_count}")
+            self.passed_text.set(f"Passed: {passed_count}")
 
     def open_files_folder(self) -> None:
         if(os.name == "nt"):
