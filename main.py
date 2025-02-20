d8737fd2a4fcf8f80f24b40c5be0b3c32df19aee
unknown <Isaac Boots>
2025-02-20 13:32:49 -0800

    Added multiprocessing.freeze_support() to main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Author: Isaac J. Boots
 
 import gui
+import multiprocessing
 
 def main() -> None:
     app:gui.App = gui.App()
     app.run()
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     main()
