5dde56a256dc7dcca595c88ae36421919bacd3fd
CoderDude2 <isaacboots1@gmail.com>
2026-07-31 10:38:43 -0700

    Updated is_centered function to check for at least 3 centered circles. Also fixed the is_asc_ds_mismatch function to look for points within a certain radius

--- checks.py
+++ checks.py
@@ -38,8 +38,10 @@ def normalize(vector: npt.NDArray) -> npt.NDArray:
         return vector / np.sqrt(np.dot(vector, vector))
     return vector
 
+
 def distance_from_origin(point: npt.NDArray) -> float:
     return np.sqrt(np.sum(np.pow(point ,2)))
+
 
 def intersect_triangle(
     ray_origin: npt.NDArray,
@@ -83,62 +85,6 @@ def intersect_triangle(
 
     return True
 
-
-def is_centered(stl_object: stl.STLObject) -> bool:
-    x_ray_origin: npt.NDArray = np.array([20, 0, 2])
-    x_ray_direction: npt.NDArray = normalize(x_ray_origin - np.array([0, 0, 0]))
-
-    y_ray_origin: npt.NDArray = np.array([0, 20, 2])
-    y_ray_direction: npt.NDArray = normalize(y_ray_origin - np.array([0, 0, 0]))
-
-    z_ray_origin: npt.NDArray = np.array([0, 0, 20])
-    z_ray_direction: npt.NDArray = normalize(z_ray_origin - np.array([0, 0, 0]))
-
-    x_intersection: bool = False
-    y_intersection: bool = False
-    z_intersection: bool = False
-
-    for facet in stl_object.facets:
-        if not x_intersection:
-            x_intersection = intersect_triangle(
-                x_ray_origin, x_ray_direction, facet.v1, facet.v2, facet.v3
-            )
-        if not y_intersection:
-            y_intersection = intersect_triangle(
-                y_ray_origin, y_ray_direction, facet.v1, facet.v2, facet.v3
-            )
-        if not z_intersection:
-            z_intersection = intersect_triangle(
-                z_ray_origin, z_ray_direction, facet.v1, facet.v2, facet.v3
-            )
-        else:
-            return False
-
-    return x_intersection and y_intersection and not z_intersection
-
-@dataclass
-class Point3D:
-    x: np.float64
-    y: np.float64
-    z: np.float64
-
-    def __str__(self):
-        return f"({self.x}, {self.y}, {self.z})"
-
-@dataclass
-class Circle:
-    center_point: Point3D
-    radius: np.float64
-
-    def __str__(self):
-        return f"({self.center_point.x}, {self.center_point.y}) r={self.radius} z={self.center_point.z}"
-
-def distance(p1:Point3D, p2:Point3D) -> np.float64:
-    return np.sqrt(np.pow(p1.x - p2.x, 2) + np.pow(p1.y - p2.y, 2))
-
-def midpoint(p1:Point3D, p2:Point3D) -> Point3D:
-    return Point3D((p1.x + p2.x) / 2, (p1.y + p2.y) / 2, p1.z)
-
 def get_circle(p1: Point3D, p2: Point3D, p3: Point3D) -> Circle|None:
     D: np.float64 = 2 * ((p1.x * (p2.y - p3.y)) + (p2.x * (p3.y - p1.y)) + (p3.x * (p1.y - p2.y)))
 
@@ -172,36 +118,30 @@ def get_all_circles(stl_file: stl.STLObject) -> list[Circle]:
     circles = {}
 
     for key, val in zMap.items():
-        if len(val) > 24 and len(val) < 30:
+        if len(val) > 20:
             circles[key] = val
     
     result: list[Circle] = []
-
     for val in circles.values():
         cir = get_circle(val[0], val[1], val[2])
 
-        if cir is not None and round(cir.center_point.x, 2) == 0 and round(cir.center_point.x, 2) == 0:    
-            if round(cir.radius, 2) >= 1.15 and round(cir.radius, 2) < 1.3:
-                result.append(cir)
+        if cir is not None and round(cir.center_point.x, 2) == 0 and round(cir.center_point.y, 2) == 0:    
+            result.append(cir)
     
     return result
+
+def is_centered(stl_object: stl.STLObject) -> bool:
+    return len(get_all_circles(stl_object)) > 3
 
 def is_asc_ds_mistmatch(stl_path: str|Path) -> bool:
     if type(stl_path) is str:
         stl_path = Path(stl_path)
 
     stl_file:stl.STLObject = stl.open_stl_file(stl_path)
-    max_z_index_cir: Circle|None = None
-    for cir in get_all_circles(stl_file):
-        if max_z_index_cir is None:
-            max_z_index_cir = cir
-            continue
-        
-        if cir.center_point.z > max_z_index_cir.center_point.z:
-            max_z_index_cir = cir
-    if max_z_index_cir:
-        if "TA" in stl_file.name and max_z_index_cir.radius > 1.17 and max_z_index_cir.radius < 1.3:
+    for point in stl_file.points:
+        if distance_from_origin(point[0:2]) < 0.73:
             return True
+
     return False
 
 def in_circle(stl_file: stl.STLObject, radius: int) -> bool:
